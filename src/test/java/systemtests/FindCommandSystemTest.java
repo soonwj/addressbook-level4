4b6d2545b45afe40178ebcd845d5af3f0dc52452
Siddharth Madhavan <siddharth.madhavan@gmail.com>
2017-10-11 17:16:42 +0800

    added find by tags

--- src/test/java/systemtests/FindCommandSystemTest.java
+++ src/test/java/systemtests/FindCommandSystemTest.java
@@ -126,7 +126,7 @@ public class FindCommandSystemTest extends AddressBookSystemTest {
         /* Case: find tags of person in address book -> 0 persons found */
         List<Tag> tags = new ArrayList<>(DANIEL.getTags());
         command = FindCommand.COMMAND_WORD + " " + tags.get(0).tagName;
-        ModelHelper.setFilteredList(expectedModel, ALICE, BENSON, CARL, DANIEL, ELLE, FIONA, GEORGE );
+        ModelHelper.setFilteredList(expectedModel, ALICE, BENSON, CARL, DANIEL, ELLE, FIONA, GEORGE);
         assertCommandSuccess(command, expectedModel);
         assertSelectedCardUnchanged();
 
