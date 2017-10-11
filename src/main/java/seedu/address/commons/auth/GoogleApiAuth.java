a94ccd41cb9a83bd65808836deb122c22f77260e
philemontan <philemontan@gmail.com>
2017-10-11 16:12:55 +0800

    GoogleApiAuth.java: Created with basic functionality implemented

--- src/main/java/seedu/address/commons/auth/GoogleApiAuth.java
+++ src/main/java/seedu/address/commons/auth/GoogleApiAuth.java
@@ -1,7 +1,104 @@
 package seedu.address.commons.auth;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeTokenRequest;
+import com.google.api.client.googleapis.auth.oauth2.GoogleBrowserClientRequestUrl;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+
+import java.io.IOException;
+import java.util.Arrays;
+
 /**
+ * License Crediting: Code adapted from : https://developers.google.com/people/v1/getting-started
  * Created by Philemon1 on 11/10/2017.
+ * This class is used to facilitate the OAuth2 process with Google's APIs
+ * Client ID and Secret used belong to the creator (Philemon) 's project CS2103T-addressbook
+ * Client ID name: CS2103T-Doc
+ * https://console.developers.google.com/apis/credentials?project=cs2103t-addressbook
  */
 public class GoogleApiAuth {
+    /**
+     * Class Constants
+     */
+    private static String clientId = "591065149112-d03t233mjnjbc7v7uev56ubeghbmoamg.apps.googleusercontent.com";
+    private static String clientSecret = "waKCYfZT6DJqnh6qsK0ReEWW";
+    private static String contactWriteScope = "https://www.googleapis.com/auth/contacts";
+    private static String contactReadOnlyScope = "https://www.googleapis.com/auth/contacts.readonly";
+    private static String redirectUrl = "";
+
+    /**
+     * Class Attributes
+     */
+    private GoogleTokenResponse authToken;
+    private GoogleCredential credential;
+    private HttpTransport httpTransport;
+    private JacksonFactory jsonFactory;
+
+    /**
+     * Default constructor
+     */
+    public GoogleApiAuth(){
+        httpTransport = new NetHttpTransport();
+        jsonFactory = new JacksonFactory();
+    }
+
+    public GoogleTokenResponse getAuthToken() {
+        return authToken;
+    }
+
+    public GoogleCredential getCredential() {
+        return credential;
+    }
+
+    /**
+     * Returns a URL for the caller to set in webview, enabling end-user to authenticate/login.
+     * Scope of permissions: Google Contacts Read/Write
+     * @return a String URL for caller to redirect.
+     */
+    public String getAuthContactWriteUrl() {
+        return new GoogleBrowserClientRequestUrl(clientId, redirectUrl, Arrays.asList(contactWriteScope)).build();
+    }
+
+    /**
+     * Fully Sets up the GoogleApiAuth instance when a valid authCode is input.
+     * Once this method has executed successfully, this instance's credentials can be accessed internally or externally
+     * for HTTP calls to the Google People API, using Google API client libraries.
+     * @param authCode
+     * @return boolean indicating if set-up is successful or not
+     */
+    public boolean setupCredentials(String authCode) {
+        try {
+            setAuthToken(authCode);
+        } catch(IOException e) {
+            return false;
+        }
+        // Input parameter is valid, attempt to generate credentials now
+        credential = new GoogleCredential.Builder()
+                    .setTransport(httpTransport)
+                    .setJsonFactory(jsonFactory)
+                    .setClientSecrets(clientId, clientSecret)
+                    .build()
+                    .setFromTokenResponse(authToken);
+        return true;
+    }
+
+    /**
+     * Helper method called by setupCredentials(), to obtain authToken from user input authCode
+     * @param authCode
+     * @throws IOException
+     */
+    private void setAuthToken(String authCode) throws IOException {
+        if(authCode == null) {
+            throw new IOException("authCode cannnot be null");
+        }
+        authToken =  new GoogleAuthorizationCodeTokenRequest(
+                httpTransport, jsonFactory, clientId, clientSecret, authCode, redirectUrl)
+                .execute();
+    }
+
+
+
 }
