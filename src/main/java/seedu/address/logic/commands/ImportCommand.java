6f6728a13785930a9d945654d22cab677cdf866d
philemontan <philemontan@gmail.com>
2017-10-11 23:22:16 +0800

    AddressBookParser.java: Recognizes import now. ImportCommand.java: New Command type created

---
+++ src/main/java/seedu/address/logic/commands/ImportCommand.java
@@ -0,0 +1,20 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+
+/**
+ * Created by Philemon1 on 11/10/2017.
+ */
+public class ImportCommand extends Command{
+
+    public static final String COMMAND_WORD = "import";
+    public String MESSAGE_SUCCESS = "Import Success";
+
+    public ImportCommand(){}
+
+
+    @Override
+    public CommandResult execute() throws CommandException {
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
