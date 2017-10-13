4af455ba40e36e786ab24b6a1bdf8d7060184f4f
philemontan <philemontan@gmail.com>
2017-10-13 17:30:35 +0800

    checkstyleTest corrections

--- src/test/java/seedu/address/commons/util/GooglePersonConverterUtilTest.java
+++ src/test/java/seedu/address/commons/util/GooglePersonConverterUtilTest.java
@@ -1,58 +1,61 @@
 package seedu.address.commons.util;
 
-import com.google.api.services.people.v1.model.Person;
-import com.google.api.services.people.v1.model.Address;
-import com.google.api.services.people.v1.model.PhoneNumber;
-import com.google.api.services.people.v1.model.EmailAddress;
-import com.google.api.services.people.v1.model.Name;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+import com.google.api.services.people.v1.model.Address;
+import com.google.api.services.people.v1.model.EmailAddress;
+import com.google.api.services.people.v1.model.Name;
+import com.google.api.services.people.v1.model.Person;
+import com.google.api.services.people.v1.model.PhoneNumber;
+
 import seedu.address.commons.exceptions.InvalidGooglePersonException;
-import java.util.ArrayList;
-import static org.junit.Assert.assertEquals;
 
 /**
  * Unit testing for the GooglePersonConverterUtil class
  * Checks that the converter handles exceptions gracefully, and sets default values correctly
  */
 public class GooglePersonConverterUtilTest {
-    private GooglePersonConverterUtil convertTool = new GooglePersonConverterUtil();
-
     /**
      * Verifies the conversion result for Google Person with null parameters
      */
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
+    private GooglePersonConverterUtil convertTool = new GooglePersonConverterUtil();
     @Test
-    public void convertGooglePersonWithNullNameException() throws InvalidGooglePersonException{
-        Person testPerson = getGooglePerson(null, "12345678", "blk001 Test Ave"
-                , "test@gmail.com");
+    public void convertGooglePersonWithNullNameException() throws InvalidGooglePersonException {
+        Person testPerson = getGooglePerson(null, "12345678", "blk001 Test Ave",
+                "test@gmail.com");
         thrown.expect(InvalidGooglePersonException.class);
         GooglePersonConverterUtil.convertPerson(testPerson);
     }
 
     @Test
-    public void convertGooglePersonWithNullPhoneException() throws InvalidGooglePersonException{
-        Person testPerson = getGooglePerson("Testee", null, "blk001 Test Ave"
-                , "test@gmail.com");
+    public void convertGooglePersonWithNullPhoneException() throws InvalidGooglePersonException {
+        Person testPerson = getGooglePerson("Testee", null, "blk001 Test Ave",
+                "test@gmail.com");
         thrown.expect(InvalidGooglePersonException.class);
         GooglePersonConverterUtil.convertPerson(testPerson);
     }
 
     @Test
-    public void convertGooglePersonWithNullEmail() throws InvalidGooglePersonException{
-        Person testPerson = getGooglePerson("Testee", "12345678", "blk001 Test Ave"
-                , null);
+    public void convertGooglePersonWithNullEmail() throws InvalidGooglePersonException {
+        Person testPerson = getGooglePerson("Testee", "12345678", "blk001 Test Ave",
+                null);
         seedu.address.model.person.Person convertedPerson = GooglePersonConverterUtil.convertPerson(testPerson);
         assertEquals(convertedPerson.getEmail().toString(), GooglePersonConverterUtil.DEFAULT_EMAIL);
     }
 
     @Test
-    public void convertGooglePersonWithNullAddress() throws InvalidGooglePersonException{
-        Person testPerson = getGooglePerson("Testee", "12345678", null
-                , "test@gmail.com");
+    public void convertGooglePersonWithNullAddress() throws InvalidGooglePersonException {
+        Person testPerson = getGooglePerson("Testee", "12345678", null,
+                "test@gmail.com");
         seedu.address.model.person.Person convertedPerson = GooglePersonConverterUtil.convertPerson(testPerson);
         assertEquals(convertedPerson.getAddress().toString(), GooglePersonConverterUtil.DEFAULT_ADDRESS);
     }
