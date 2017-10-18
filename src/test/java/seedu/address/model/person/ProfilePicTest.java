1fe916ae45f16a077c68408d4ab98fa71ab5a25e
NEW-PA1Q6R2O9MA\Wei Jun <e0032329@u.nus.edu>
2017-10-18 22:22:49 +0800

    Updated ProfilePic isValidUrl() + Added tests for ProfilePic, UpdateProfilePicCommand and UpdateProfilePicParser

---
+++ src/test/java/seedu/address/model/person/ProfilePicTest.java
@@ -0,0 +1,21 @@
+package seedu.address.model.person;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_LOCAL_IMAGE_URL;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_WEB_IMAGE_URL;
+
+import org.junit.Test;
+
+public class ProfilePicTest {
+    @Test
+    public void isValidUrl() {
+        // invalid URL
+        assertFalse(ProfilePic.isValidUrl("")); // empty string
+        assertFalse(ProfilePic.isValidUrl("images/fail.png")); // malformed URL
+
+        // valid addresses
+        assertTrue(ProfilePic.isValidUrl(VALID_WEB_IMAGE_URL)); //web URL
+        assertTrue(ProfilePic.isValidUrl(VALID_LOCAL_IMAGE_URL)); //local URL
+    }
+}
