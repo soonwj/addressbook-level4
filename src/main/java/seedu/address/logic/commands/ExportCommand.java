9288d93ba01d126afc8b368aa601d17e342cb213
Philemon Tan <philemontan@gmail.com>
2017-10-20 12:47:36 +0800

    ExportCommand.java: initiated new command

---
+++ src/main/java/seedu/address/logic/commands/ExportCommand.java
@@ -0,0 +1,10 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+
+public class ExportCommand extends Command {
+    @Override
+    public CommandResult execute() throws CommandException {
+        return null;
+    }
+}
