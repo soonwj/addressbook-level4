0c7c1e96c935e5adf87aa75825703458f56f394c
philemontan <philemontan@gmail.com>
2017-10-21 02:05:29 +0800

    Oauth2Command.java: preliminary completion in abstract logic

--- src/main/java/seedu/address/logic/commands/Oauth2Command.java
+++ src/main/java/seedu/address/logic/commands/Oauth2Command.java
@@ -1,27 +1,46 @@
 package seedu.address.logic.commands;
 
-import seedu.address.commons.core.EventsCenter;
+import java.io.IOException;
 
-/**This class is the parent class for all commands requiring OAuth2 authentication using the BrowserPanel
+import seedu.address.commons.core.EventsCenter;
+import seedu.address.commons.events.ui.Oauth2BrowserRequestEvent;
+
+
+/**This class is the parent class for all commands requiring OAuth2 authentication using the BrowserPanel.
+ * This class cannot be instantiated, as it is requires child classes with a defined COMMAND_TYPE.
  * Created by Philemon1 on 21/10/2017.
  */
 public abstract class Oauth2Command extends Command {
-    //Subclass have to define COMMAND_TYPE
-    protected String COMMAND_TYPE;
-    private final String REDIRECT_URL = "https://cs2103tdummyendpoint.herokuapp.com";
+    public static final String INVALID_COMMAND_TYPE_MESSAGE = "The COMMAND_TYPE cannot be null";
+    private static final String REDIRECT_URL = "https://cs2103tdummyendpoint.herokuapp.com";
+    protected final String commandType;
+
+    protected Oauth2Command(String inputType) throws IOException {
+        if (inputType == null) {
+            throw new IOException(INVALID_COMMAND_TYPE_MESSAGE);
+        }
+        commandType = inputType;
+    }
+    protected Oauth2Command() throws IOException {
+        throw new IOException(INVALID_COMMAND_TYPE_MESSAGE);
+    }
 
     public String getRedirectUrl() {
         return REDIRECT_URL;
     }
 
     /**
-     * Child classes are expected to define their own COMMAND_TYPE, as the browser panel
-     * will trigger events contextually based on it
+     * Main common functionality for all Oauth2Command childs classes. Fires an event intended for the BrowserPanel,
+     * triggering it to start the UI authentication process within the BrowserPanel
+     * @throws IOException
      */
-    protected abstract void setCommandType();
-
-    protected void triggerBrowserAuth() {
-//        EventsCenter.getInstance().post();
+    protected void triggerBrowserAuth() throws IOException {
+        try {
+            Oauth2BrowserRequestEvent trigger = new Oauth2BrowserRequestEvent(commandType);
+        } catch (IOException E) {
+            throw E;
+        }
+        EventsCenter.getInstance().post(new Oauth2BrowserRequestEvent(commandType));
     }
 
 }
