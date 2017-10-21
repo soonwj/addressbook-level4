4da5f9d3742b0450415da398232b132a6fb4b352
philemontan <philemontan@gmail.com>
2017-10-22 05:12:00 +0800

    GoogleExportedCompleteEvent.java: Added new event to fire to browserpanel upon completion of export

---
+++ src/main/java/seedu/address/commons/events/logic/GoogleExportCompleteEvent.java
@@ -0,0 +1,19 @@
+package seedu.address.commons.events.logic;
+
+import seedu.address.commons.events.BaseEvent;
+
+/**
+ * Created by Philemon1 on 22/10/2017.
+ */
+public class GoogleExportCompleteEvent extends BaseEvent {
+    private String exportedContactsViewUrl;
+
+    public GoogleExportCompleteEvent(String inputExportedContactsViewUrl) {
+        exportedContactsViewUrl = inputExportedContactsViewUrl;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+}
