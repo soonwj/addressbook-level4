a22b50c0631e210fbdf76f1d1424890e45b57580
Royce <e0030923@u.nus.edu>
2017-10-23 12:52:30 +0800

    Sort Events by date

---
+++ src/main/java/seedu/address/model/util/EventDateComparator.java
@@ -0,0 +1,22 @@
+package seedu.address.model.util;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Comparator;
+
+import seedu.address.model.person.event.Event;
+
+public class EventDateComparator implements Comparator<Event> {
+
+    @Override
+    public int compare(Event e1, Event e2) {
+        ZoneId sgt = ZoneId.of("GMT+8");
+        LocalDate currentDate = LocalDate.now(sgt);
+        if (e1.getEventDate().eventLocalDate.isBefore(currentDate)
+                || e2.getEventDate().eventLocalDate.isBefore(currentDate)) {
+            return e2.getEventDate().eventLocalDate.compareTo(e1.getEventDate().eventLocalDate);
+        } else {
+            return e1.getEventDate().eventLocalDate.compareTo(e2.getEventDate().eventLocalDate);
+        }
+    }
+}
