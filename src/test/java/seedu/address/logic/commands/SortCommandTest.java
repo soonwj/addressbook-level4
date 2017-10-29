611d2fb1f70be657b8342d21a519aa5d402b1908
Siddharth Madhavan <siddharth.madhavan@gmail.com>
2017-10-30 06:09:55 +0800

    added test cases for sort function

---
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -0,0 +1,36 @@
+package seedu.address.logic.commands;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.Test;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.UndoRedoStack;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+public class SortCommandTest {
+
+    @Test
+    public void executeSortOnEmptyAddressBookSuccess() {
+        Model model = new ModelManager();
+        assertCommandSuccess(prepareCommand(model), model, SortCommand.COMMAND_SUCCESS, model);
+    }
+
+    @Test
+    public void executeSortOnNonEmptyAddressBookSuccess() {
+        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        assertCommandSuccess(prepareCommand(model), model, SortCommand.COMMAND_SUCCESS, model);
+    }
+
+    /**
+     * Generates a new {@code ClearCommand} which upon execution, clears the contents in {@code model}.
+     */
+    private SortCommand prepareCommand(Model model) {
+        SortCommand command = new SortCommand();
+        command.setData(model, new CommandHistory(), new UndoRedoStack());
+        return command;
+    }
+}
