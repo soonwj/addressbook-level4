e8995c20573b7d2e22cb3e3e9dd347ae429b4564
Siddharth Madhavan <siddharth.madhavan@gmail.com>
2017-10-29 21:26:55 +0800

    updated email test cases

--- src/test/java/seedu/address/logic/commands/EmailCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EmailCommandTest.java
@@ -1,30 +1,24 @@
 package seedu.address.logic.commands;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static seedu.address.commons.core.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.EmailCommand.MESSAGE_EMAIL_APP;
 import static seedu.address.logic.commands.EmailCommand.MESSAGE_NOT_SENT;
-import static seedu.address.testutil.TypicalPersons.CARL;
-import static seedu.address.testutil.TypicalPersons.ELLE;
-import static seedu.address.testutil.TypicalPersons.FIONA;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 import org.junit.Test;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.UndoRedoStack;
-import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
-import seedu.address.model.person.ReadOnlyPerson;
+import seedu.address.model.person.NameContainsKeywordsPredicate;;
 
 /**
  * Contains integration tests (interaction with the Model) for {@code EmailCommand}.
@@ -60,17 +54,17 @@ public class EmailCommandTest {
     }
 
     @Test
-    public void execute_zeroKeywords_noPersonFound() {
+    public void execute_noPersonFound() {
         String expectedMessage = String.format(MESSAGE_NOT_SENT);
         EmailCommand command = prepareCommand(" ");
-        assertCommandSuccess(command, expectedMessage, Collections.emptyList());
+        assertCommandSuccess(command, model, expectedMessage, model);
     }
 
     @Test
     public void execute_multipleKeywords_multiplePersonsFound() {
         String expectedMessage = String.format(MESSAGE_EMAIL_APP);
-        EmailCommand command = prepareCommand("Kurz Elle Kunz");
-        assertCommandSuccess(command, expectedMessage, Arrays.asList(CARL, ELLE, FIONA));
+        EmailCommand command = prepareCommand("email to/Kurz Elle Kunz");
+        assertCommandSuccess(command, model, expectedMessage, model);
     }
 
     /**
@@ -81,20 +75,5 @@ public class EmailCommandTest {
                 new EmailCommand(new NameContainsKeywordsPredicate(Arrays.asList(userInput.split("\\s+"))),"","");
         command.setData(model, new CommandHistory(), new UndoRedoStack());
         return command;
-    }
-
-    /**
-     * Asserts that {@code command} is successfully executed, and<br>
-     *     - the command feedback is equal to {@code expectedMessage}<br>
-     *     - the {@code FilteredList<ReadOnlyPerson>} is equal to {@code expectedList}<br>
-     *     - the {@code AddressBook} in model remains the same after executing the {@code command}
-     */
-    private void assertCommandSuccess(EmailCommand command, String expectedMessage, List<ReadOnlyPerson> expectedList) {
-        AddressBook expectedAddressBook = new AddressBook(model.getAddressBook());
-        CommandResult commandResult = command.execute();
-
-        assertEquals(expectedMessage, commandResult.feedbackToUser);
-        assertEquals(expectedList, model.getFilteredPersonList());
-        assertEquals(expectedAddressBook, model.getAddressBook());
     }
 }
